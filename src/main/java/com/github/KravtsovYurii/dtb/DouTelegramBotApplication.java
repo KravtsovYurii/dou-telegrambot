a4d00389cbf681c841ad127e0d5f5a54b61ac156
KravtsovYurii <yurakravtsov31200@gmail.com>
2021-06-23 16:25:04 +0300

    DTB-0: added SpringBoot skeleton to the project.

---
+++ src/main/java/com/github/KravtsovYurii/dtb/DouTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.KravtsovYurii.dtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/KravtsovYurii/dtb/DouTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.KravtsovYurii.dtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DouTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
